038a6077af70aae20fb4958883a12e3475999963
Lakshmikanth Raju <lraju@vmware.com>
2017-09-12 11:53:47 +0530

    updated delete folder function, renamed files

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Subtitles Match
-
+### subtitles-match
 Renames subtitles to video's name to support automatic subtitle loading
 Removes unwanted sample videos, zip files and images
 Clubs videos of same series together for easy access
--- delete-interm.py
+++ delete-interm.py
@@ -8,14 +8,15 @@ def removeNpmBower(path):
     #bower_components,node_modules
     print path
     try:
-        os.remove(path)
+        #os.remove(path)
+        shutil.rmtree(path)
     except WindowsError as err:
         print err
 
 def processDirectory(dir):
     for root, directories, filenames in os.walk(dir):
         for d in directories:
-            if d == "bower_components":# or d == "node_modules":
+            if d == "bower_components" or d == "node_modules":
                 removeNpmBower(os.path.join(root, d))
 
 if __name__ == "__main__":
